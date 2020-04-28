905b4e6e29280c3640bb043d15c281d463ab60d3
GESAlkimi <graci_smith@hotmail.com>
2020-04-28 11:34:27 -0400

    typo

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const NotFound = () => {
     return (
         <div>
-            <h1>Not Fuond</h1>
+            <h1>Not Found</h1>
             <p className="lead">The page you're looking for does not exist</p>
         </div>
     )
